11ad698a2f9c375438e74b2137a6392f68af9cd2
Carlos Jennings <74273012+carlosjennings1991@users.noreply.github.com>
2021-03-21 18:39:12 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -27,7 +27,7 @@ plot(mpg, ground_clearance, main="Clearance to MPG", xlab="MPG", ylab="Ground Cl
 abline(lm(ground_clearance~mpg), col="red")
 
 ## Fifth summary: AWD to MPG
-plot(mpg, AWD, main="Clearance to MPG", xlab="MPG", ylab="AWD", pch=19)
+plot(mpg, AWD, main="AWD to MPG", xlab="MPG", ylab="AWD", pch=19)
 abline(lm(AWD~mpg), col="red")
 
 ## Sixth summary: MPG to MPG
@@ -39,4 +39,36 @@ plot(mpg, mpg, main="MPG to MPG (perfect correlation", xlab="MPG", ylab="MPG", p
 ## Perform a multiple line regression and view the statistical summary
 all_variables_mlr <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg_df)
 summary(all_variables_mlr)
+
+
+## Deliverable 2 Below: 
+
+## Read in the CSV
+suspension_coil_df = read.csv("Suspension_Coil.csv")
+
+## Create a the total summary data frame
+total_summary <- suspension_coil_df %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI) ** 2,SD=sd(PSI))
+
+## Create the lot summary data frame, which is like the df above but grouped by Lot Size
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=sd(PSI) ** 2,SD=sd(PSI))
+
+
+## Deliverable 3 Below: 
+
+# Create a sample from the population data
+sample_table <- suspension_coil_df %>% sample_n(50)
+
+# Compare the sample to the population using a t-test
+t.test(log10(sample_table$PSI),mu=mean(log10(suspension_coil_df$PSI)))
+
+# Now compare each lot's PSI metrics to the Population
+# Step 1: Create a data frame for each lot
+lot1_df <- subset(suspension_coil_df, Manufacturing_Lot=='Lot1')
+lot2_df <- subset(suspension_coil_df, Manufacturing_Lot=='Lot2')
+lot3_df <- subset(suspension_coil_df, Manufacturing_Lot=='Lot3')
+
+# Step 2: Compare each lot to the Population
+t.test(log10(lot1_df$PSI),mu=mean(log10(suspension_coil_df$PSI)))
+t.test(log10(lot2_df$PSI),mu=mean(log10(suspension_coil_df$PSI)))
+t.test(log10(lot3_df$PSI),mu=mean(log10(suspension_coil_df$PSI)))
 
